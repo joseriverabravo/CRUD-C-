6cb84f125b51d9a50df2e124f1bda9ed28d3bd26
joseriverabravo <joseeriverabravo@gmail.com>
2019-08-14 19:39:36 -0400

    version 1

---
+++ CRUD/Controllers/TablaController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD.Models;
+using CRUD.Models.ViewModels;
+namespace CRUD.Controllers
+{
+    public class TablaController : Controller
+    {
+        // GET: Tabla
+        public ActionResult Index()
+        {
+            List<ListaTablaViewModel> lst;
+            using (masterEntities1 db = new masterEntities1())
+            {
+                
+                 lst = (from d in db.Tabla
+                           select new ListaTablaViewModel
+                           {
+                               Id = d.id,
+                               Nombre = d.nombre,
+                               Correo = d.correo,
+                               Fecha = d.fecha
+                           }).ToList();
+
+}
+                return View(lst);
+        }
+
+        public ActionResult Nuevo ()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Nuevo(TablaViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (masterEntities1 db =new masterEntities1())
+                    {
+                        var otabla = new Tabla();
+                        otabla.id = model.Id;
+                        otabla.correo = model.Correo;
+                        otabla.fecha = model.Fecha;
+                        otabla.nombre = model.Nombre;
+                       
+                        db.Tabla.Add(otabla);
+                        db.SaveChanges();
+
+                    }
+                    return Redirect("/");
+                }
+                return View(model);
+
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+
+        }
+
+    }
+}
---
+++ CRUD/Models/ViewModels/TablaViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace CRUD.Models.ViewModels
+{
+    public class TablaViewModel
+    {
+        [Required]
+        
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name ="Nombre")]
+        public String Nombre { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Correo Electronico")]
+        public String Correo { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Fecha")]
+        public String Fecha { get; set; }
+        
+    }
+}
